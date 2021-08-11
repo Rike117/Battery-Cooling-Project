dc6faad5f7c33e9ec434485e6a3894ecf63e7717
Enrique Lopez <86859561+Rike117@users.noreply.github.com>
2021-08-10 23:17:34 -0700

    Update Bat_Sim_C_None.m

--- Bat_Sim_C_None.m
+++ Bat_Sim_C_None.m
@@ -3,14 +3,15 @@ function[total_field_time,up_time,Total_Flight_Time,Total_Cost] = Bat_Sim_C_None
 Charger_Cost = costs(1);
 Battery_Cost = costs(2);
 Work_Cost = costs(3);
+Method_Cost = costs(4);
 num_batteries=0;
 Avg_Flight_Time = batt(3);
 
 b= min_flight_time*60/Avg_Flight_Time;
-Total_Cost = (b*Battery_Cost)+(0*Charger_Cost)+(Work_Cost);
 Total_Flight_Time = (b*Avg_Flight_Time)/60;
 total_field_time = Total_Flight_Time;
-up_time = 1;
+Total_Cost = (b*Battery_Cost)+(0*Charger_Cost)+(Work_Cost*total_field_time);
+up_time = 100;
 
 
 %  filename = "\Bat_Sim_C_None" + num_batteries+"B.xlsx";
